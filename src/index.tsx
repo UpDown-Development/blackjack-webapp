df5915e06d950650dbc6731a25cd6810c4dd0785
Ethan Bettenga <bettet01@luther.edu>
2020-10-23 23:12:27 -0500

    finished setup hopefully

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import 'normalize.css';
+import App from "./containers/App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById("root")
 );
