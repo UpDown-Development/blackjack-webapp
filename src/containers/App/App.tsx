da7786e3df55997e9f1c6f4ba2d84ba464774c19
Ethan Bettenga <bettet01@luther.edu>
2020-10-25 15:38:00 -0500

    added animation

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../redux/rootReducer";
-import {initBlackJack} from "../../redux/actions/blackJackActions";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/rootReducer";
+import { initBlackJack } from "../../redux/actions/blackJackActions";
 import pips from "../../images/resources/4_pips.png";
 import styles from "./app.module.scss";
+import { motion } from 'framer-motion';
 
 function App() {
   const dispatch = useDispatch();
@@ -22,16 +23,17 @@ function App() {
   return (
     <div>
       <div className={styles.container}>
-        <img className={styles.pips} src={pips}/>
-        <h1 className={styles.title}>Blackjack</h1>
+        <motion.img initial={{y: -350}} animate={{y: 60}} transition={{ duration: 2}} className={styles.pips} src={pips} />
+        <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2 }} className={styles.title}>Blackjack</motion.h1>
       </div>
       <div className={styles.buttonContainer}>
-        <button className={styles.button}>Stats</button>
-        <button className={styles.button}>Play</button>
-        <button style={{fontSize: '22px'}} className={styles.button}>Customize</button>
+        <motion.button initial={{y: 250}} animate={{y: -50}} transition={{ duration: 5 }} className={styles.button}>Stats</motion.button>
+        <motion.button initial={{y: 200}} animate={{y: 0}} transition={{ duration: 5 }} className={styles.button}>Play</motion.button>
+        <motion.button initial={{y: 300}} animate={{y: -50}} transition={{ duration: 5 }} style={{ fontSize: "22px" }} className={styles.button}>
+          Customize
+        </motion.button>
       </div>
     </div>
-
   );
 }
 
