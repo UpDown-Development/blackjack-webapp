886369a9237e688fb17be1dd3127ada6d41cfa2e
Ethan Bettenga <bettet01@luther.edu>
2020-10-25 15:39:22 -0500

    small animation change

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -4,7 +4,7 @@ import { RootState } from "../../redux/rootReducer";
 import { initBlackJack } from "../../redux/actions/blackJackActions";
 import pips from "../../images/resources/4_pips.png";
 import styles from "./app.module.scss";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,13 +23,46 @@ function App() {
   return (
     <div>
       <div className={styles.container}>
-        <motion.img initial={{y: -350}} animate={{y: 60}} transition={{ duration: 2}} className={styles.pips} src={pips} />
-        <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2 }} className={styles.title}>Blackjack</motion.h1>
+        <motion.img
+          initial={{ y: -350 }}
+          animate={{ y: 60 }}
+          transition={{ duration: 2 }}
+          className={styles.pips}
+          src={pips}
+        />
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className={styles.title}
+        >
+          Blackjack
+        </motion.h1>
       </div>
       <div className={styles.buttonContainer}>
-        <motion.button initial={{y: 250}} animate={{y: -50}} transition={{ duration: 5 }} className={styles.button}>Stats</motion.button>
-        <motion.button initial={{y: 200}} animate={{y: 0}} transition={{ duration: 5 }} className={styles.button}>Play</motion.button>
-        <motion.button initial={{y: 300}} animate={{y: -50}} transition={{ duration: 5 }} style={{ fontSize: "22px" }} className={styles.button}>
+        <motion.button
+          initial={{ y: 250 }}
+          animate={{ y: -50 }}
+          transition={{ duration: 3 }}
+          className={styles.button}
+        >
+          Stats
+        </motion.button>
+        <motion.button
+          initial={{ y: 200 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 3 }}
+          className={styles.button}
+        >
+          Play
+        </motion.button>
+        <motion.button
+          initial={{ y: 300 }}
+          animate={{ y: -50 }}
+          transition={{ duration: 3 }}
+          style={{ fontSize: "22px" }}
+          className={styles.button}
+        >
           Customize
         </motion.button>
       </div>
