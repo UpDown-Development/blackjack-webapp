4e937ade138b480126121400f8db626381944fbf
Ethan Bettenga <bettet01@luther.edu>
2020-10-26 14:32:52 -0500

    no idea what changed but let's go

--- src/containers/BlackJackGame/BlackJackGame.tsx
+++ src/containers/BlackJackGame/BlackJackGame.tsx
@@ -1,26 +1,28 @@
-import React, {useEffect} from "react";
-import {shallowEqual, useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../redux/rootReducer";
-import {initBlackJack} from "../../redux/actions/blackJackActions";
-import {Card} from "../../models/generic";
+import React, { useEffect } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/rootReducer";
+import { initBlackJack } from "../../redux/actions/blackJackActions";
+import { Card } from "../../models/generic";
 
 const BlackJackGame = () => {
   const dispatch = useDispatch();
-  const blackjackState = useSelector((state: RootState) => state.BlackJackReducer, shallowEqual);
+  const blackjackState = useSelector(
+    (state: RootState) => state.BlackJackReducer,
+    shallowEqual
+  );
 
   useEffect(() => {
-    dispatch(initBlackJack(2))
-  }, [])
+    dispatch(initBlackJack(2));
+  }, []);
 
   return (
     <div>
       <button onClick={() => dispatch(initBlackJack(2))}>Shuffle</button>
       {blackjackState.deck.map((card: Card) => {
-        return (
-          <img style={{height: '70px'}} src={card.img}/>
-        )
+        return <img style={{ height: "70px" }} src={card.img} />;
       })}
-    </div>);
+    </div>
+  );
 };
 
 export default BlackJackGame;
