3c0ca9e9b465b029a02273143f3e9034d1563240
Ethan Bettenga <bettet01@luther.edu>
2020-11-30 21:08:25 -0600

    added else path test to Home

--- src/containers/Home/Home.test.tsx
+++ src/containers/Home/Home.test.tsx
@@ -2,6 +2,7 @@ import { failTest, setup } from "../../setupTests";
 import { genericState, players } from "../../utils/testData";
 import React from "react";
 import Home from "./Home";
+import UserReducer from "../../redux/reducers/UserReducers/UserReducer";
 
 describe("<Home/>", () => {
   it("renders without crashing", async () => {
@@ -9,6 +10,18 @@ describe("<Home/>", () => {
       jest.mock("firebase");
       const testObj = setup(genericState, <Home />);
       await expect(testObj).toBeTruthy();
+    } catch (e) {
+      failTest(e);
+    }
+  });
+  it("renders login page else statement", async () => {
+    try {
+      jest.mock("firebase");
+      const testObj = setup(
+        { genericState, UserReducer: { ...UserReducer, user: null } },
+        <Home />
+      );
+      expect(testObj.wrapper?.text()).toEqual("Card.ioLoginSignup");
     } catch (e) {
       failTest(e);
     }
